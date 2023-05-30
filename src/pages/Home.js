3eae2a86a1a67b2519bee5b8dd33c698291dc300
kngyen2507 <ntknguyen2507@gmail.com>
2023-05-30 11:50:37 +0700

    test don't use firebase 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,8 +78,7 @@ function Home () {
                                                 <img 
                                                     width="300" 
                                                     height="300"
-                                                    crossorigin="anonymous"
-                                                    src={process.env.REACT_APP_HOST + '/' + product.image} 
+                                                    src={product.firebase} 
                                                     alt='image product'
                                                 ></img> :
                                                 <img 
